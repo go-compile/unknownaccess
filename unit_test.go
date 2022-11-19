2d2206609e488d290374a97cbb20e650a5614013
go-compile <97609133+go-compile@users.noreply.github.com>
2022-11-19 14:47:43 +0000

    ci: add unit test encrypt/decrypt with encode/decode and check 3 secrets

--- unit_test.go
+++ unit_test.go
@@ -42,35 +42,93 @@ func TestEncryptDecrypt2Secrets(t *testing.T) {
 	secret2 := []byte("This is secret number 2.")
 
 	if err := block.Encrypt("password1", secret1); err != nil {
-		if err := block.Encrypt("password2", secret2); err != nil {
-			panic(err)
-		}
+		panic(err)
+	}
+	if err := block.Encrypt("password2", secret2); err != nil {
+		panic(err)
+	}
 
-		marshaled, err := block.Marshal()
-		if err != nil {
-			panic(err)
-		}
+	marshaled, err := block.Marshal()
+	if err != nil {
+		panic(err)
+	}
 
-		b2, err := unknownaccess.Unmarshal(marshaled)
-		if err != nil {
-			panic(err)
-		}
+	b2, err := unknownaccess.Unmarshal(marshaled)
+	if err != nil {
+		panic(err)
+	}
 
-		data1, err := b2.Decrypt("password1")
-		if err != nil {
-			panic(err)
-		}
+	data1, err := b2.Decrypt("password1")
+	if err != nil {
+		panic(err)
+	}
 
-		data2, err := b2.Decrypt("password2")
-		if err != nil {
-			panic(err)
-		}
+	data2, err := b2.Decrypt("password2")
+	if err != nil {
+		panic(err)
+	}
 
-		if !bytes.Equal(secret1, data1) {
-			t.Fatal("secrets do not match")
-		}
-		if !bytes.Equal(secret2, data2) {
-			t.Fatal("secrets do not match")
-		}
+	if !bytes.Equal(secret1, data1) {
+		t.Fatal("secrets do not match")
+	}
+	if !bytes.Equal(secret2, data2) {
+		t.Fatal("secrets do not match")
+	}
+}
+
+func TestEncryptDecrypt3Secrets(t *testing.T) {
+	block := unknownaccess.NewBlock()
+
+	secret1 := []byte("This is secret number 1.")
+	secret2 := []byte("This is secret number 2.")
+	secret3 := []byte("This is secret number 3.")
+
+	if err := block.Encrypt("password1", secret1); err != nil {
+		panic(err)
+	}
+
+	if err := block.Encrypt("password2", secret2); err != nil {
+		panic(err)
+	}
+
+	if err := block.Encrypt("password3", secret3); err != nil {
+		panic(err)
+	}
+
+	marshaled, err := block.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	b2, err := unknownaccess.Unmarshal(marshaled)
+	if err != nil {
+		panic(err)
+	}
+
+	data1, err := b2.Decrypt("password1")
+	if err != nil {
+		panic(err)
+	}
+
+	data2, err := b2.Decrypt("password2")
+	if err != nil {
+		panic(err)
+	}
+
+	data3, err := b2.Decrypt("password3")
+	if err != nil {
+		panic(err)
+	}
+
+	if !bytes.Equal(secret1, data1) {
+		t.Fatal("secrets do not match")
+	}
+
+	if !bytes.Equal(secret2, data2) {
+		t.Fatal("secrets do not match")
+	}
+
+	if !bytes.Equal(secret3, data3) {
+		t.Fatal("secrets do not match")
 	}
 }
