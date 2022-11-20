6cc617857f1e9f9c1bd0f9a72b466758248724d1
Go Compile <97609133+go-compile@users.noreply.github.com>
2022-11-20 14:43:47 +0000

    refactor(pkcs5): removed generics to support pre 1.18 go versions

--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module unknownaccess
 
-go 1.18
+go 1.12
 
 require golang.org/x/crypto v0.3.0
-
-require golang.org/x/sys v0.2.0 // indirect
--- pkcs5.go
+++ pkcs5.go
@@ -2,7 +2,7 @@ package unknownaccess
 
 //pkcs5 returns the padding buffer based on weather or not it is required
 // Maximum blocksize is 255
-func pkcs5[num int64 | int | int32 | uint32 | uint64](actual num, blockSize num) []byte {
+func pkcs5(actual int, blockSize int) []byte {
 	n := byte(blockSize - actual%blockSize)
 
 	// create buf of len N full of bytes equal to value of N
